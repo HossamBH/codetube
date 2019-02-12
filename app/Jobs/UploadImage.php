fcd0746a7f9e63f7c028a9fad87a758b36e77dcf
HossamEL-Sayed <hossamhamed94.hh@gmail.com>
2019-02-12 13:01:14 +0200

    part12-2

---
+++ app/Jobs/UploadImage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use File;
+use Image;
+use Storage;
+use App\Models\Channel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class UploadImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $channel;
+    public $fileId;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Channel $channel, $fileId)
+    {
+        $this->channel = $channel;
+        $this->fileId = $fileId;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $path = storage_path() . '/uploads/' . $this->fileId;
+        $fileName = $this->fileId . '.png';
+
+        Image::make($path)->encode('png')->fit(40, 40, function($c){
+
+            $c->upsize();
+        })->save();
+
+        $this->channel->image_filename = $fileName;
+        $this->channel->save();
+    }
+}
